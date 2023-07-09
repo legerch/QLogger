b4390280fc3ffad56ed0ce319837fb13d895a5fe
Charlie Leger <charlie.leger98@gmail.com>
2023-07-09 15:27:25 +0200

    [logh] Add documentation for QLogHandler

--- src/qlogger/qloghandler.cpp
+++ src/qlogger/qloghandler.cpp
@@ -4,6 +4,16 @@
 /* Class documentations      */
 /*****************************/
 
+/*!
+ * \class QLogger::QLogHandler
+ * \brief Class use to handle all log messages.
+ *
+ * This class is used to properly handle log files rotation.
+ *
+ * \sa init()
+ * \sa QLogger::QLogMsg
+ */
+
 /*****************************/
 /* Enum documentations       */
 /*****************************/
@@ -36,16 +46,56 @@ namespace QLogger
 /*         Class             */
 /*****************************/
 
+/*!
+ * \brief Construct a QLogHandler object
+ * \details
+ * This method perform \b nothing
+ */
 QLogHandler::QLogHandler()
 {
     /* Nothing to do */
 }
 
+/*!
+ * \brief Destructor of QLogHandler instance
+ * \details
+ * This method will close current log file
+ * (if any!) *
+ */
 QLogHandler::~QLogHandler()
 {
     closeFile();
 }
 
+/*!
+ * \brief Use to configure log handler
+ *
+ * \param[in] logFilename
+ * Path of log file to use. \n
+ * \em Examples:
+ * - <tt>customlog.txt</tt>: Will create log messages
+ * in current directory and use \c customlog as pattern
+ * to use for the rotation
+ * - <tt>logs/customlog.txt: Will create custom log messages
+ * in \c logs directory and use \c customlog as pattern
+ * to use for the rotation
+ * \param[in] maxFiles
+ * Number of file to use for rotation. \n
+ * For example, if set to \c 3, no more than 3
+ * files will be used for rotation.
+ * \param[in] maxFileSize
+ * Maximum log file size in bytes.
+ * \param[in] enableConsole
+ * Set to \c true to enable console/terminal printing (helpful
+ * during development, useless in \em release mode)
+ *
+ * \return
+ * Return \c true if initialization succeed. \n
+ * This method can failed if unable to open first log message
+ * (right access, hard drive full, etc...)
+ *
+ * \sa desinit()
+ */
 bool QLogHandler::init(const QString &logFilename, int maxFiles, qint64 maxFileSize, bool enableConsole)
 {
     /* Fill logger basic informations */
@@ -75,22 +125,64 @@ bool QLogHandler::init(const QString &logFilename, int maxFiles, qint64 maxFileS
     return true;
 }
 
+/*!
+ * \brief Use to release (and close) all used ressources
+ * \details
+ * This method will close all used ressources and restore
+ * default log behaviour.
+ *
+ * \sa init()
+ */
 void QLogHandler::desinit()
 {
     closeFile();
 }
 
+/*!
+ * \brief Use to get current unique instance
+ * of QLogHandler
+ *
+ * \return
+ * Return current unique instance
+ * of QLogHandler
+ */
 QLogHandler &QLogHandler::instance()
 {
     static QLogHandler instance;
     return instance;
 }
 
+/*!
+ * \brief Use to verify than log handler
+ * is initialized
+ * \details
+ * To know initialization status, this method will
+ * verify if log file is currently open or not.
+ *
+ * \return
+ * Return \c true if log handler is initialized
+ */
 bool QLogHandler::isInitialized() const
 {
     return m_currentFile.isOpen();
 }
 
+/*!
+ * \brief Use to generate filepath of a log
+ * file based on needed index
+ * \details
+ * An index of 0 will use filepath pattern defined
+ * with init() method. \n
+ * In case of any other indexes, filepath pattern will
+ * still be used but a number will be added before
+ * file extension.
+ *
+ * \param[in] index
+ * File index to use
+ *
+ * \return
+ * Return pathfile to use for choosen index
+ */
 QString QLogHandler::generateFilepath(int index) const
 {
     QString filename = m_fileBasename;
@@ -104,6 +196,26 @@ QString QLogHandler::generateFilepath(int index) const
     return m_currentDir.absoluteFilePath(filename + "." + m_fileExt);
 }
 
+/*!
+ * \brief Use to open file with write access
+ *
+ * \param[in] filepath
+ * Path to file to open. \n
+ * If fill doesn't exist, it will be created.
+ * \param[in] truncate
+ * Set to \c true to clear any previous data of
+ * the file.
+ *
+ * \note
+ * This method do not verify than previous
+ * stream file has been closed before opening
+ * another.
+ *
+ * \return
+ * Return \c true if operation succeed.
+ *
+ * \sa closeFile()
+ */
 bool QLogHandler::openFile(const QString &filepath, bool truncate)
 {
     /* Assign file */
@@ -130,6 +242,14 @@ bool QLogHandler::openFile(const QString &filepath, bool truncate)
     return true;
 }
 
+/*!
+ * \brief Use to close current log file
+ * \details
+ * This method will close (and flush) current log
+ * file (only if file is currently open)
+ *
+ * \sa openFile()
+ */
 void QLogHandler::closeFile()
 {
     if(m_currentFile.isOpen()){
@@ -138,6 +258,20 @@ void QLogHandler::closeFile()
     }
 }
 
+/*!
+ * \brief Use to verify if current log
+ * file will reach file limit size with this
+ * message.
+ *
+ * \param[in] logMsg
+ * Log message to check.
+ *
+ * \return
+ * Return \c true if adding \c logMsg to current log
+ * file will reach file limit size.
+ *
+ * \sa rotateFiles()
+ */
 bool QLogHandler::sizeFileLimitIsReached(const QLogMsg &logMsg) const
 {
     const qsizetype nextFileSize = m_currentFileSize + logMsg.getSizeInBytes();
@@ -150,6 +284,19 @@ bool QLogHandler::sizeFileLimitIsReached(const QLogMsg &logMsg) const
     return false;
 }
 
+/*!
+ * \brief Use to rotate log files
+ * \details
+ * This method will:
+ * - Close current log file (to flush any pending data)
+ * - Rename "logFile N" to "logFile N+1"
+ * - Open next log file to write into
+ *
+ * \return
+ * Return \c true if rotation file succeed.
+ *
+ * \sa sizeFileLimitIsReached()
+ */
 bool QLogHandler::rotateFiles()
 {
     /* Close current file */
@@ -176,7 +323,22 @@ bool QLogHandler::rotateFiles()
     return succeed;
 }
 
-//TODO: doc (oldName must exist)
+/*!
+ * \brief Use this method to rename a file
+ *
+ * \param[in] oldName
+ * Source file to rename. \n
+ * This filepath must exist, otherwise renaming
+ * will failed.
+ * \param[in] newName
+ * New name of the file. \n
+ * If this file exist, it will be removed before proceeding
+ * to rename operation.
+ *
+ * \return
+ * Return \c true if \c oldName has been renamed to
+ * \c newName
+ */
 bool QLogHandler::renameFile(const QString &oldName, const QString &newName)
 {
     /* Remove target file is exists */
@@ -188,6 +350,22 @@ bool QLogHandler::renameFile(const QString &oldName, const QString &newName)
     return QFile::rename(oldName, newName);
 }
 
+/*!
+ * \brief Main log handler operation which
+ * gonna "proceed" the log message
+ * \details
+ * This method will:
+ * - Check if message can be added to current
+ * log file (if not, files will be rotated
+ * - Write log to file (and console if enable)
+ *
+ * \param[in] idType
+ * Type of log message
+ * \param[in] context
+ * Log context informations
+ * \param[in] msg
+ * Log message to create
+ */
 void QLogHandler::proceedMessage(QtMsgType idType, const QMessageLogContext &context, const QString &msg)
 {
     QMutexLocker locker(&m_mutex);
@@ -210,12 +388,38 @@ void QLogHandler::proceedMessage(QtMsgType idType, const QMessageLogContext &con
     m_currentFileSize += fmtMsg.getSizeInBytes();
 }
 
+/*!
+ * \brief Static method used to register
+ * into qInstallMessageHandler() method.
+ * \details
+ * For more details, please se Qt documentation at:
+ * https://doc.qt.io/qt-5/qtglobal.html#qInstallMessageHandler
+ *
+ * \param[in] idType
+ * Type of log message
+ * \param[in] context
+ * Log context informations
+ * \param[in] msg
+ * Log message to create
+ */
 void QLogHandler::messageHandler(QtMsgType idType, const QMessageLogContext &context, const QString &msg)
 {
     QLogHandler &logHandler = QLogHandler::instance();
     logHandler.proceedMessage(idType, context, msg);
 }
 
+/*!
+ * \brief Use to print log message to console/terminal
+ * \details
+ * Only \em QtDebugMsg and \c QtInfoMsg will be written
+ * on \c stdout output, any other type will be written
+ * to \c stderr.
+ *
+ * \param[in] idType
+ * Type of log message
+ * \param[in] fmtMsg
+ * Formatted message to print.
+ */
 void QLogHandler::messageToConsole(QtMsgType idType, const QLogMsg &fmtMsg)
 {
     switch(idType)
