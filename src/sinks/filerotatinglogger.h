d66cc82385c07a36a965bd7926b2ee4307f7ed43
Charlie LEGER <c.leger@borea-dental.com>
2024-05-31 16:54:09 +0200

    [sinks:rotate] Create class that allow to rotate multiple log files

---
+++ src/sinks/filerotatinglogger.h
@@ -0,0 +1,34 @@
+#ifndef FILEROTATINGLOGGER_H
+#define FILEROTATINGLOGGER_H
+
+#include "filelogger.h"
+
+namespace QLogger
+{
+
+class FileRotatingLogger : public FileLogger
+{
+
+public:
+    FileRotatingLogger(const QFileInfo &file, int maxFiles, qint64 maxSize);
+
+protected:
+    virtual void proceedEntry(const QLogMsg &msg) override;
+
+private:
+    bool rotateFiles();
+
+private:
+    QString generateBasename(int index) const;
+    QString generateFilepath(int index) const;
+
+private:
+    int m_maxFileNb;
+    qint64 m_maxFileSize;
+
+    QString m_fileBasename;
+};
+
+} // Namespace QLogger
+
+#endif // FILEROTATINGLOGGER_H
