e90c205837b342982685d0aaef94864b203f3309
Charlie LEGER <c.leger@borea-dental.com>
2024-05-31 16:53:25 +0200

    [sinks:file] Create class to manage a log file

---
+++ src/sinks/filelogger.cpp
@@ -0,0 +1,128 @@
+#include "filelogger.h"
+
+/*****************************/
+/* Class documentations      */
+/*****************************/
+
+/*****************************/
+/* Enum documentations       */
+/*****************************/
+
+/*****************************/
+/* Structure documentations  */
+/*****************************/
+
+/*****************************/
+/* Signals documentations    */
+/*****************************/
+
+/*****************************/
+/* Macro definitions         */
+/*****************************/
+
+/*****************************/
+/* Start namespace           */
+/*****************************/
+
+namespace QLogger{
+
+/*****************************/
+/* Constants definitions     */
+/*****************************/
+
+/*****************************/
+/* Functions implementation  */
+/*         Class             */
+/*****************************/
+
+FileLogger::FileLogger(const QDir &dir, const QString &extension)
+{
+    /* Set properties */
+    m_dir = dir;
+    m_extension = extension;
+
+    /* Create directory if needed */
+    if(!m_dir.exists()){
+        m_dir.mkpath(".");
+    }
+}
+
+bool FileLogger::openFile(const QString &basename, bool truncate)
+{
+    /* Assign file */
+    const QFileInfo filePath(m_dir, basename + m_extension);
+    m_file.setFileName(filePath.absoluteFilePath());
+
+    /* Set open flags */
+    QFile::OpenMode openFlags = QIODevice::WriteOnly | QIODevice::Text;
+    if(truncate){
+        openFlags |= QIODevice::Truncate;
+    }else{
+        openFlags |= QIODevice::Append;
+    }
+
+    /* Open it and prepare stream */
+    bool succeed = m_file.open(openFlags);
+    if(!succeed){
+        m_fileSize = 0;
+        return false;
+    }
+
+    m_fileSize = m_file.size();
+    m_fileStream.setDevice(&m_file);
+
+    return true;
+}
+
+void FileLogger::closeFile()
+{
+    m_file.close();
+    m_fileSize = 0;
+}
+
+QString FileLogger::getFilePath(const QString &basename) const
+{
+    return m_dir.absoluteFilePath(basename + '.' + m_extension);
+}
+
+qsizetype FileLogger::getFileSize() const
+{
+    return m_fileSize;
+}
+
+qsizetype FileLogger::getFileSizeNext(const QLogMsg &msg) const
+{
+    return m_fileSize + msg.getSizeInBytes();
+}
+
+void FileLogger::proceedEntry(const QLogMsg &msg)
+{
+    m_fileStream << msg;
+    m_fileSize = getFileSizeNext(msg);
+}
+
+bool FileLogger::renameFile(const QString &oldName, const QString &newName)
+{
+    /* Remove target file is exists */
+    if(QFile::exists(newName)){
+        QFile::remove(newName);
+    }
+
+    /* Rename file */
+    return QFile::rename(oldName, newName);
+}
+
+/*****************************/
+/* Functions implementation  */
+/*        Structures         */
+/*****************************/
+
+/*****************************/
+/* End namespace             */
+/*****************************/
+
+} // namespace QLogger
+
+/*****************************/
+/* End file                  */
+/*****************************/
---
+++ src/sinks/filelogger.h
@@ -0,0 +1,44 @@
+#ifndef FILELOGGER_H
+#define FILELOGGER_H
+
+#include "base/baselogger.h"
+
+#include <QDir>
+#include <QTextStream>
+
+namespace QLogger
+{
+
+class FileLogger : public BaseLogger
+{
+
+public:
+    FileLogger(const QDir &dir, const QString &extension);
+
+protected:
+    bool openFile(const QString &basename, bool truncate);
+    void closeFile();
+
+protected:
+    QString getFilePath(const QString &basename) const;
+    qsizetype getFileSize() const;
+    qsizetype getFileSizeNext(const QLogMsg &msg) const;
+
+protected:
+    virtual void proceedEntry(const QLogMsg &msg) override;
+
+protected:
+    static bool renameFile(const QString &oldName, const QString &newName);
+
+private:
+    QDir m_dir;
+    QString m_extension;
+
+    QFile m_file;
+    qsizetype m_fileSize;
+    QTextStream m_fileStream;
+};
+
+} // Namespace QLogger
+
+#endif // FILELOGGER_H
