5dd45ed6c4a32ee815908c5fd6f3f81ccb975f1b
Charlie LEGER <c.leger@borea-dental.com>
2024-08-02 15:24:40 +0200

    [log:entry] Create class which will contain all log infos

---
+++ src/log/logentry.h
@@ -0,0 +1,40 @@
+#ifndef LOGENTRY_H
+#define LOGENTRY_H
+
+#include <QFileInfo>
+#include <QMessageLogContext>
+
+namespace QLogger
+{
+
+class LogEntry
+{
+
+public:
+    explicit LogEntry(QtMsgType idType, const QMessageLogContext &context, const QString &msg);
+
+public:
+    QtMsgType getType() const;
+    QString getTypeString() const;
+
+    bool contextIsAvailable() const;
+    const QFileInfo& getCtxFile() const;
+    int getCtxLine() const;
+    const QString& getCtxFctSig() const;
+    QString getCtxFctName() const;
+
+    const QString& getMsg() const;
+
+private:
+    QtMsgType m_idType;
+    QString m_msg;
+
+    bool m_haveContext;
+    QFileInfo m_ctxFile;
+    int m_ctxLine;
+    QString m_ctxFctSig;
+};
+
+} // Namespace QLogger
+
+#endif // LOGENTRY_H
