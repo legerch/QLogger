e14319770b3bd73aa13afff4d40a8344e37cc12b
Charlie Leger <charlie.leger98@gmail.com>
2024-08-19 11:41:05 +0200

    [docs] Complete missing documentation

--- qlogger/qloggerfactory.cpp
+++ qlogger/qloggerfactory.cpp
@@ -33,6 +33,33 @@ QLoggerFactory::QLoggerFactory()
     /* Nothing to do */
 }
 
+/*!
+ * \brief Use to configure rotating file log sink
+ *
+ * \param[in] file
+ * Path of log file to use. \n
+ * \em Examples:
+ * - <tt>customlog.txt</tt>: Will create log messages
+ * in current directory and use \c customlog as pattern
+ * to use for the rotation
+ * - <tt>logs/customlog.txt: Will create custom log messages
+ * in \c logs directory and use \c customlog as pattern
+ * to use for the rotation
+ * \param[in] maxFiles
+ * Number of file to use for rotation. \n
+ * For example, if set to \c 3, no more than 3
+ * files will be used for rotation.
+ * \param[in] maxFileSize
+ * Maximum log file size in bytes.
+ * \param[in] enableConsole
+ * Set to \c true to enable console/terminal printing (helpful
+ * during development, useless in \em release mode)
+ *
+ * \note
+ * If any sinks already exist, it will be properly deleted.
+ *
+ * \sa desinit()
+ */
 void QLoggerFactory::initLoggerRotating(const QFileInfo &file, int maxFiles, qint64 maxSize, bool enableConsole)
 {
     m_logger = std::make_unique<FileRotatingLogger>(file, maxFiles, maxSize, enableConsole);
